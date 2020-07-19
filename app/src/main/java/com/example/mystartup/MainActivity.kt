d165f8f949814398338f66264cb62134683a9b78
kia3415 <54172475+kia3415@users.noreply.github.com>
2020-07-20 00:14:05 +0900

    set bottom navigation activity
    bottom navigation
    : back, home, favorite

--- app/src/main/java/com/example/mystartup/MainActivity.kt
+++ app/src/main/java/com/example/mystartup/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.mystartup
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.mystartup.ui.favorite.FavoriteFragment
+import com.example.mystartup.ui.home.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var favoriteFragment: FavoriteFragment
+    lateinit var ft:FragmentTransaction
+    lateinit var fm: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+
     }
+
+    private fun init() {
+        //fragment 생성
+        homeFragment = HomeFragment()
+        favoriteFragment = FavoriteFragment()
+
+        //fragment manager
+        fm = supportFragmentManager
+        ft = fm.beginTransaction()
+
+        //set homefragment
+        ft.replace(R.id.frame_layout,homeFragment).commit()
+        bottom_navigation_view.selectedItemId = R.id.navigation_home
+
+        //navigation listener
+        bottom_navigation_view.setOnNavigationItemSelectedListener(object:BottomNavigationView.OnNavigationItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when(item.itemId){
+                    R.id.navigation_back->{//이전
+
+                    }
+                    R.id.navigation_home->{//홈
+                        ft = fm.beginTransaction()
+                        ft.replace(R.id.frame_layout,homeFragment).commit()
+                    }
+                    R.id.navigation_favorite->{//즐겨찾기
+                        ft = fm.beginTransaction()
+                        ft.replace(R.id.frame_layout,favoriteFragment).commit()
+                    }
+                }
+                return true
+            }
+        })
+
+    }
+
+
 }
