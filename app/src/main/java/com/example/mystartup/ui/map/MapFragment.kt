81e3dc45a86f84c7a7b2ec8361057b7325102f3c
DAEHYEON <jdhqpt12@gmail.com>
2020-07-28 02:18:30 +0900

    Show Google Map

--- app/src/main/java/com/example/mystartup/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/mystartup/ui/map/MapFragment.kt
@@ -6,37 +6,67 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mystartup.R
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment :Fragment(){
+class MapFragment :Fragment(),OnMapReadyCallback{
     lateinit var mMap:GoogleMap
+    lateinit var mapView:MapView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.google_map,container,false)
-        /*val mapView=view.findViewById<MapView>(R.id.map)
-        mapView.getMapAsync(this)*/
-        return inflater.inflate(R.layout.google_map,container,false)
+        mapView=view.findViewById(R.id.google_map_view)as MapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+        return view
     }
 
-    /*override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(p0: GoogleMap?) {
         mMap = p0!!
         val marker = LatLng(37.502976, 126.980316)
         mMap.addMarker(
             MarkerOptions().position(marker).title("똥작역")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
         )
-        //mMap.addMarker(MarkerOptions().position(marker)
-        // .title("혜령이똥").icon(BitmapDescriptorFactory.fromResource(R.drawable.common_google_signin_btn_icon_dark)))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15F))
-    }*/
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+
 }
