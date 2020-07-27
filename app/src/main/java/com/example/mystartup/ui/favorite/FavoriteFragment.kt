48d98d40b887f84b8b2ef7db222a9931a8a7e734
kia3415 <54172475+kia3415@users.noreply.github.com>
2020-07-27 13:32:46 +0900

    set FavoriteFragment
    add viewpager at FavoriteFragment

--- app/src/main/java/com/example/mystartup/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/mystartup/ui/favorite/FavoriteFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mystartup.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.fragment_favorite.*
 
 class FavoriteFragment : Fragment() {
 
+    lateinit var adapter: ViewPagerAdapter
+    var tabTitle = arrayListOf<String>("채용","공간","교육","자금")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,5 +22,25 @@ class FavoriteFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_favorite, container, false)
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        adapter = ViewPagerAdapter(
+            requireContext(),
+            tabTitle
+        )
+        viewpager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewpager, object : TabLayoutMediator.TabConfigurationStrategy{
+            override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                tab.setText(tabTitle[position])
+            }
+        }).attach()
     }
 }
