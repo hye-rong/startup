87ffde754bf2bca8a634f886408ed9ebab53b901
DAEHYEON <jdhqpt12@gmail.com>
2020-07-28 21:59:05 +0900

    AsyncTask

---
+++ app/src/main/java/com/example/mystartup/ui/map/CafeAsyncTask.kt
@@ -0,0 +1,19 @@
+package com.example.mystartup.ui.map
+
+import android.os.AsyncTask
+import android.util.Log
+import java.net.HttpURLConnection
+import java.net.URL
+
+/*
+class CafeAsyncTask : AsyncTask<Any?, Any?, Any?>() {
+    override fun doInBackground(vararg params: Any?): Any? {
+        val urlString="http://openapi.seoul.go.kr:8088/75624659416a64683131306d686f4a6c/json/jobCafeOpenInfo/1/80/"
+        val url=URL(urlString)
+        val connection: HttpURLConnection=url.openConnection() as HttpURLConnection
+
+        connection.requestMethod="GET"
+        Log.d("requestProperty",connection.)
+    }
+
+}*/
